8a974059e6c9351c4dfe6c84e6721adec40631b8
Zachary Lessner <LessnerZachary@gmail.com>
2019-07-02 15:11:10 -0400

    update

--- blackjack.py
+++ blackjack.py
@@ -1,21 +1,5 @@
 import random
 
-# cards= [
-#     {"2": 2},
-#     {"3": 3},
-#     {"4": 4},
-#     {"5": 5},
-#     {"6": 6},
-#     {"7": 7},
-#     {"8": 8},
-#     {"9": 9},
-#     {"10": 10},
-#     {"J": 10},
-#     {"Q": 10},
-#     {"K": 10},
-#     {"A": 1},
-# ]
-
 J=10
 Q=10
 K=10
@@ -41,69 +25,82 @@ stack= numDecks * deck
 
 random.shuffle(stack)
 
-player1=[]
+while True:
 
-dealer=[]
+    play = str(input("Would you like to play the blackjack hand? y/n "))
 
-player1Total=0
-dealerTotal=0
+    if play == 'y' or play == 'Y':
 
-player1.extend((stack.pop(), stack.pop()))
+        player1=[]
 
-dealer.append(stack.pop())
+        dealer=[]
 
+        player1Total=0
+        dealerTotal=0
 
-while sum(dealer)<17:
-    if sum(player1)>=17:
+        player1.extend((stack.pop(), stack.pop()))
+
         dealer.append(stack.pop())
+
+
+        while sum(dealer)<17:
+            if sum(player1)>=17:
+                dealer.append(stack.pop())
+            else:
+                player1.append(stack.pop())
+
+        if sum(player1)>21:
+            bankRoll=bankRoll-minBet
+        elif sum(dealer)>21:
+            bankRoll=bankRoll+minBet
+        elif sum(player1)<sum(dealer):
+            bankRoll=bankRoll-minBet
+        elif sum(player1)==sum(dealer):
+            bankRoll=bankRoll
+        else: 
+            bankRoll=bankRoll+minBet
+                
+                
+
+
+
+
+
+
+
+
+
+
+        for x in player1:
+            if x in smallCards:
+                count +=1
+
+            if x in bigCards:
+                count -=1
+
+
+        for x in dealer:
+            if x in smallCards:
+                count +=1
+
+            if x in bigCards:
+                count -=1
+
+
+
+
+
+        print (player1)
+        print (sum(player1))
+        print (dealer)
+        print (sum(dealer))
+        print (count)
+        print (bankRoll)
+
+    
+    elif play == "N" or play =="n":
+        break
+    
     else:
-        player1.append(stack.pop())
-
-if sum(player1)>21:
-    bankRoll=bankRoll-minBet
-elif sum(dealer)>21:
-    bankRoll=bankRoll+minBet
-elif sum(player1)<sum(dealer):
-    bankRoll=bankRoll-minBet
-elif sum(player1)==sum(dealer):
-    bankRoll=bankRoll
-else: 
-    bankRoll=bankRoll+minBet
-        
-        
-
-
-
-
-
-
-
-
-
-
-for x in player1:
-    if x in smallCards:
-        count +=1
-
-    if x in bigCards:
-        count -=1
-
-
-for x in dealer:
-    if x in smallCards:
-        count +=1
-
-    if x in bigCards:
-        count -=1
-
-
-
-
-
-print (player1)
-print (sum(player1))
-print (dealer)
-print (sum(dealer))
-print (count)
-print (bankRoll)
+        print("Please enter y/n")
 
