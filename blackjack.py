4b89fc8265fd16a44327c774dbf8ed113f6871e9
Zachary Lessner <LessnerZachary@gmail.com>
2019-07-19 13:56:37 -0400

    logic updates

--- blackjack.py
+++ blackjack.py
@@ -59,14 +59,21 @@ for i in range(10000):
         if bankRoll<=0:
             break
 
-        bet=5
+        if bankRoll>=5:
+            bet=5
+        else:
+            print ("Bet is remainder of bankroll")
+            bet=bankRoll
 
         if count/numDecks>=3:
             bet= bet*4
+            print ("Bet quadrupled due to true count")
         elif count/numDecks>=2:
             bet= bet*3
+            print ("Bet trippled due to true count")
         elif count/numDecks>=1:
             bet= bet*2
+            print ("Bet doubled due to true count")
         else:
             bet=bet
 
@@ -94,10 +101,14 @@ for i in range(10000):
         goodbye=True
         howdy=True
         yo=True
+        more=True
+        evenMore=True
         open=False
         close=False
         open2=False
         close2=False
+        close3=False
+        open3=False
         aceList=[]
         aceList2=[]
         playerList=[]
@@ -106,8 +117,8 @@ for i in range(10000):
 
         for x in player1:
             playerTotal= playerTotal + sum(list(x.values())) 
-        print("shalom")
-        print(playerTotal)
+        # print("shalom")
+        # print(playerTotal)
 
         for y in dealer:
             dealerTotal= dealerTotal + sum(list(y.values()))
@@ -139,6 +150,10 @@ for i in range(10000):
             global close2
             global playerList
             global otherTotal
+            global more
+            global close3
+            global evenMore
+            global open3
 
             # print(moo, "line ", cf.f_lineno)
             # print(oink, "line ", cf.f_lineno)
@@ -156,8 +171,8 @@ for i in range(10000):
                     else:
                         otherTotal=otherTotal + sum(list(h.values())) 
                 
-                print ("hola")
-                print (otherTotal)
+                # print ("hola")
+                # print (otherTotal)
 
                 if (sum(player1[0].values())==2 and sum(player1[1].values())==2) and (sum(dealer[0].values())<=7):
                     player1.pop()
@@ -168,6 +183,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==3 and sum(player1[1].values())==3) and (sum(dealer[0].values())<=7):
                     player1.pop()
@@ -178,6 +194,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==4 and sum(player1[1].values())==4) and (sum(dealer[0].values())==5 or sum(dealer[0].values())==6):
                     player1.pop()
@@ -188,6 +205,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==6 and sum(player1[1].values())==6) and (sum(dealer[0].values())<=6):
                     player1.pop()
@@ -198,6 +216,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==7 and sum(player1[1].values())==7) and (sum(dealer[0].values())<=7):
                     player1.pop()
@@ -208,6 +227,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==8 and sum(player1[1].values())==8):
                     player1.pop()
@@ -218,6 +238,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==9 and sum(player1[1].values())==9) and (sum(dealer[0].values())<=6 or sum(dealer[0].values())==8 or sum(dealer[0].values())==9):
                     player1.pop()
@@ -228,6 +249,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
                 elif (sum(player1[0].values())==11 and sum(player1[1].values())==11):
                     player1.pop()
@@ -238,6 +260,7 @@ for i in range(10000):
                     print ("split")
                     print (sum(player1[0].values()))
                     bet=bet*2
+                    print ("Bet doubled due to split")
                     continue
 
                 # woofList=[]
@@ -259,6 +282,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif playerList.count('A')==1 and (playerList.count('2')==1 or playerList.count('3')==1) and len(playerList)==2 and (dealerTotal==5 or dealerTotal==6):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     if playerTotal>21:
@@ -271,6 +295,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif playerList.count('A')==1 and (playerList.count('4')==1 or playerList.count('5')==1) and len(playerList)==2 and (dealerTotal==4 or dealerTotal==5 or dealerTotal==6):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     if playerTotal>21:
@@ -283,6 +308,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif playerList.count('A')==1 and (playerList.count('6')==1 or playerList.count('7')==1) and len(playerList)==2 and (dealerTotal==3 or dealerTotal==4 or dealerTotal==5 or dealerTotal==6):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     if playerTotal>21:
@@ -325,6 +351,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif (playerTotal==11 and len(player1)==2 and dealerTotal!=11):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     while dealerTotal<17:
@@ -335,6 +362,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif (playerTotal==10 and len(player1)==2) and (dealerTotal!=10 and dealerTotal!=11):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     while dealerTotal<17:
@@ -345,6 +373,7 @@ for i in range(10000):
                     playerTotal= playerTotal + sum(player1[-1].values())
                 elif (playerTotal==9 and len(player1)==2) and (dealerTotal==3 or dealerTotal==4 or dealerTotal==5 or dealerTotal==6):
                     bet=bet*2
+                    print ("Bet doubled due to doubling down")
                     player1.append(stack.pop())
                     playerTotal= playerTotal + sum(player1[-1].values())
                     while dealerTotal<17:
@@ -362,9 +391,9 @@ for i in range(10000):
                     for x in player1:
                         for number in x:
                             aceList2.append(number)
-                    print(aceList2)
-                    print ('hi')
-                    print (aceList2.count('A'))
+                    # print(aceList2)
+                    # print ('hi')
+                    # print (aceList2.count('A'))
                     for i in range(len(player1)):
                         front=list(player1[i].keys())
                         back=list(player1[i].values())
@@ -374,9 +403,9 @@ for i in range(10000):
                                 # for value in list(player1[i]):
                                 playerTotal=playerTotal-10
                                 # player1[i][value] = 1
-                                print ("player total below")
-                                print (playerTotal)
-                                print("woof woof") 
+                                # print ("player total below")
+                                # print (playerTotal)
+                                # print("woof woof") 
                             game=False
                             continue
 
@@ -399,6 +428,13 @@ for i in range(10000):
                             if goodbye==True or howdy==True:
                                 close=True
                             continue
+
+                        if aceList2.count('A')==5 and evenMore==True:
+                            playerTotal=playerTotal-10
+                            evenMore=False
+                            if goodbye==True or howdy==True or yo==True:
+                                open3=True
+                            continue
                         
                         if open==True:
                             playerTotal=playerTotal-10
@@ -408,6 +444,11 @@ for i in range(10000):
                         if close==True:
                             playerTotal=playerTotal-10
                             close=False
+                            continue
+
+                        if open3==True:
+                            playerTotal=playerTotal-10
+                            evenMore=False
                             continue
 
                         else:
@@ -452,9 +493,9 @@ for i in range(10000):
                     for x in dealer:
                         for number in x:
                             aceList.append(number)
-                    print(aceList)
-                    print ('hi')
-                    print (aceList.count('A'))
+                    # print(aceList)
+                    # print ('hi')
+                    # print (aceList.count('A'))
 
                     if aceList.count('A')==2 and woof==True:
                         dealerTotal=dealerTotal-10
@@ -490,6 +531,18 @@ for i in range(10000):
                             dealerTotal= dealerTotal + sum(dealer[-1].values())
                         continue
 
+                    if aceList.count('A')==5 and more==True:
+                        dealerTotal=dealerTotal-10
+                        more=False
+                        if meow==True or woof==True or roar==True:
+                                close3=True
+                        if dealerTotal<=21 and dealerTotal>=17:            
+                            break
+                        while dealerTotal<17:
+                            dealer.append(stack.pop())
+                            dealerTotal= dealerTotal + sum(dealer[-1].values())
+                        continue
+
                     if open2==True:
                         dealerTotal=dealerTotal-10
                         open2=False
@@ -503,6 +556,16 @@ for i in range(10000):
                     if close2==True:
                         dealerTotal=dealerTotal-10
                         close2=False
+                        if dealerTotal<=21 and dealerTotal>=17:            
+                            break
+                        while dealerTotal<17:
+                            dealer.append(stack.pop())
+                            dealerTotal= dealerTotal + sum(dealer[-1].values())
+                        continue
+                    
+                    if close3==True:
+                        dealerTotal=dealerTotal-10
+                        close3=False
                         if dealerTotal<=21 and dealerTotal>=17:            
                             break
                         while dealerTotal<17:
@@ -529,16 +592,22 @@ for i in range(10000):
 
         if (playerTotal==21 and dealerTotal!=21 and len(player1)==2):
             bankRoll=bankRoll+(bet*1.5)
+            print("Player wins hand, blackjack pays off 3 to 2")
         elif playerTotal>21:
             bankRoll=bankRoll-bet
+            print("Dealer wins hand")
         elif dealerTotal>21:
             bankRoll=bankRoll+bet
+            print("Player wins hand")
         elif playerTotal<dealerTotal:
             bankRoll=bankRoll-bet
+            print("Dealer wins hand")
         elif playerTotal==dealerTotal:
             bankRoll=bankRoll
+            print("Hand pushes")
         else: 
             bankRoll=bankRoll+bet
+            print("Player wins hand")
 
             
 
@@ -584,9 +653,9 @@ for i in range(10000):
         print (playerTotal)
         print(f'Dealer hand: {moo}')
         print (dealerTotal)
-        print(f'True count: {count}')
+        print(f'New true count: {count}')
         print(f'Bankroll: ${bankRoll}')
-        print ("Next hand")      
+        print ('\n' + "Next hand" + '\n')      
 
 
 
